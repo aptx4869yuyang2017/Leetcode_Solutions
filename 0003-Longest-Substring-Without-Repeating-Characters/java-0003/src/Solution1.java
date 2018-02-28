7252de0a33ccbc77b918ac4081b8e867da5ffead
yuyang <aptx4869yuyang2017@gmail.com>
2018-02-28 15:53:30 +0800

    change readme

---
+++ 0003-Longest-Substring-Without-Repeating-Characters/java-0003/src/Solution1.java
@@ -0,0 +1,17 @@
+/// Source : https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+/// Author : 禹洋
+/// Time   : 2018-2-28
+
+// Sliding Window
+// Time Complexity: O(len(s))
+// Space Complexity: O(len(charset))
+
+
+
+public class Solution1 {
+    public int lengthOfLongestSubstring(String s) {
+
+        int[] freq = new int[256]
+
+    }
+}
